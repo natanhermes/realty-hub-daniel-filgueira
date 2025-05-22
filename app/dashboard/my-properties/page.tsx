4c9142d83c5f2e93bf4dc874b397e36afabbacf3
Natã Hermes <nhermesdev@gmail.com>
2025-05-22 16:26:37 -0300

    feat: add signup button to user admin and add username info to session data

--- app/dashboard/my-properties/page.tsx
+++ app/dashboard/my-properties/page.tsx
@@ -4,21 +4,36 @@ import { listPropertiesAction } from '@/app/actions/listPropertiesAction';
 import { Property } from '@/types/Property';
 import { ListProperties } from './list-properties';
 import { Plus } from 'lucide-react';
+import { auth } from '@/auth';
 
 export default async function MyProperties() {
   const properties = await listPropertiesAction() as Property[]
+
+  const session = await auth()
+  const isAdmin = session?.user?.username === 'admin' || session?.user?.username === 'danielfilgueira';
 
   return (
     <>
       <div className='flex justify-between items-center mb-4'>
         <h1 className="text-3xl text-darkBlue font-bold">Meus imóveis</h1>
 
-        <Link href="/dashboard/my-properties/create">
-          <Button variant="outline" className='text-darkBlue'>
-            <Plus size={16} />
-            Novo
-          </Button>
-        </Link>
+        <div className='flex items-center gap-4'>
+          {isAdmin && (
+            <Link href="/arearestrita/cadastro">
+              <Button variant="outline" className='text-darkBlue'>
+                <Plus size={16} />
+                Novo usuário
+              </Button>
+            </Link>
+          )}
+
+          <Link href="/dashboard/my-properties/create">
+            <Button variant="outline" className='text-darkBlue'>
+              <Plus size={16} />
+              Novo
+            </Button>
+          </Link>
+        </div>
       </div>
 
       <ListProperties properties={properties} />
--- auth.ts
+++ auth.ts
@@ -2,6 +2,12 @@ import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { findUserByCredentials } from "./app/services/userService";
 
+declare module "next-auth" {
+    interface User {
+        username: string
+    }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Credentials({
@@ -17,14 +23,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }),
     ],
     callbacks: {
-        session: ({ session, token }) => {
-            return {
-                ...session,
-                user: {
-                    ...session.user,
-                    id: token.sub
-                }
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+                token.username = user.username
             }
-        }
+            return token
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string
+                session.user.username = token.username as string
+            }
+            return session
+        },
     }
 })
